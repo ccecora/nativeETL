86c8049b1f13072e42462df2bf83c72e6fc3a58e
Chase Cecora <cjc0885@gmail.com>
2024-09-27 17:06:39 -0400

    Create namespaces for cli commands

--- nativeETL/cli.py
+++ nativeETL/cli.py
@@ -11,7 +11,8 @@ def main():
     parser = argparse.ArgumentParser(description="nativeETL, ETL pipeline creator & executor using only Python built-in modules")
     subparsers = parser.add_subparsers(dest='command', help='Available commands')
 
-    parser_validate = subparsers.add_parser('validate', help="Validate a pipeline's configuration file to ensure it's usable.")
+    parser_validate = subparsers.add_parser('validate', help="Validate a pipeline's configuration file to ensure it's usable. \
+                                            Note that this command will be run for config-dependent CLI commands")
     parser_validate.add_argument('--config', '-c', required=True, help="Path to the configuration file.")
 
     parser_run = subparsers.add_parser('run', help='Run the pipeline based on the configuration file, irregardles of schedule.')
@@ -24,16 +25,24 @@ def main():
     parser_schedule = subparsers.add_parser('schedule', help='Initialize the scheduler on this machine to run any pipelines with \
                                        configured schedules.  Pipelines configs without a schedule or tagged as an exclusion \
                                        will not be scheduled to run can only be executed with the nativeETL run command.')
+     parser_generate.add_argument('--safe', '-s', required=False, default=True, help="If True, will ensure the project is safe \
+                                  before the scheduler is initialized and every existing config wil be validated.")
 
     args = parser.parse_args()
 
     if args.command == 'validate':
         validate_config(args.config)
     elif args.command == 'run':
+        validate_config(args.config)
         run_pipeline(args.config)
     elif args.command == 'generate':
         generate_config(args.config)
     elif args.command == 'schedule':
+        
+        if args.safe:
+            for existing_configs in config_path:
+                validate_config(config)
+
         initialize_scheduler()
     else:
         parser.print_help()
---
+++ nativeETL/commands/__init__.py
@@ -0,0 +1,4 @@
+from generate import generate_config
+from run import run_pipeline
+from generate import generate_config
+from validate import validate_config
